1d498158e30acb8109d7a8afa1e6cc85f23a77ca
jay <jay.vala@msn.com>
2018-04-17 14:49:23 +0200

    Language support added in optimized way
    More Optimizations to come

--- scrapping.py
+++ scrapping.py
@@ -1,6 +1,14 @@
 
 # coding: utf-8
 
+# **Done with english corpus**
+# 
+# *TODO:* 
+# * German, French and Spanish Corpus
+
+# In[1]:
+
+
 # Too many warnings, Beautiful Soup is not beautiful at all 
 
 import warnings
@@ -15,16 +23,28 @@ if not sys.warnoptions:
     warnings.simplefilter("ignore")
 
 
+# In[2]:
 
 
-
-# Text File with all the links, Please have the links.txt in the same folder as this script
-with open("links.txt") as file:
+# Text File with all the links
+with open("/home/jay/Thesis/links.txt") as file:
     lines = file.readlines()
 
 
+################################################################################################################
+
+download = int(input("Enter 1 for English, 2 for German, 3 for French, 4 for Spanish: \n"))
+if download == 1:
+    lang = "EN"
+elif download == 2:
+    lang = "DE"
+elif download == 3:
+    lang = "FR"
+elif download == 4:
+    lang = "ES"
 
 
+###############################################################################################################
 def topic_and_subtopic(list_links):
     """Returns the Topic and Subtopic by HTML Parsing"""
     topic = []
@@ -61,9 +81,16 @@ def topic_and_subtopic(list_links):
     return topic_str,subtopic_str
 
 
+# In[4]:
+
+
 # Creates links
 
-def link_creation(list_links):
+# TODO:
+    # Add support for language selection
+    # Currently set to manual
+    
+def link_creation(list_links, language):
     """Returns list of links to extract text from"""
     link_extract = []    # List to store all the links created
     temp_list = []       # Temporary list to store all the numbers we get from regular expressions
@@ -79,18 +106,19 @@ def link_creation(list_links):
     
     # Creating list of links to extract text from 
     for index, number in enumerate(temp_list):
-        link_extract.append("https://eur-lex.europa.eu/legal-content/EN/TXT/HTML/?uri=LEGISSUM:"+number+"&from=EN")
+        link_extract.append("https://eur-lex.europa.eu/legal-content/"+language+"/TXT/HTML/?uri=LEGISSUM:"+number+"&from="+language)
 
     return link_extract
 
 
+# In[5]:
 
 
 # Extraction function
 store_list_success = []     # List to store all the links that were successfully extracted\
 store_list_failure = []     # List to store all the links that were not successfully extracted\
 
-def extraction(hyper_links):
+def extraction(hyper_links, language):
     
     """Saves text in its native form from webpages
        Returns information regarding files """
@@ -106,18 +134,20 @@ def extraction(hyper_links):
         
     # Saving the text in appropiate folder
     homedir = os.environ['HOME']
+
+    dir_to_save = homedir+"/"+"Data_"+language+"/"+topic+"/"+subtopic
         
         
     try:
-        os.stat(homedir+"/"+"Data/"+topic+"/"+subtopic)
+        os.stat(dir_to_save)
     except:
-        os.makedirs(homedir+"/"+"Data/"+topic+"/"+subtopic)
+        os.makedirs(dir_to_save)
         
-    log_file = open(homedir+"/"+"Data/"+topic+"/"+subtopic+"/"+'log'+'.txt',"w")  #logging all the outputs to log file
+    #log_file = open(dir_to_save+"/"+'log'+'.txt',"w")  #logging all the outputs to log file
     
-    sys.stdout = log_file # Telling system to write all the print statements to log file
+    #sys.stdout = log_file # Telling system to write all the print statements to log file
     
-    for index, link in enumerate(hyper_links):
+    for i, link in enumerate(hyper_links):
         print("URL for Extraction is: {}".format(link))
         try:
             htp = urllib.request.urlopen(link)
@@ -127,7 +157,7 @@ def extraction(hyper_links):
             
             # kill all script and style elements
             for script in soup(["script", "style"]):
-                script.extract()    # rip it out
+                script.extract()    
 
             # get text
             text = soup.get_text()
@@ -143,9 +173,9 @@ def extraction(hyper_links):
             title = text.split('\n', 1)[0]
     
                     
-            print("Saving Text to the file: {}.txt".format(homedir+"/"+"Data/"+topic+"/"+subtopic+"/"+title))
+            print("Saving Text to the file: {}.txt".format(dir_to_save+"/"+title))
         
-            with open(homedir+"/"+"Data/"+topic+"/"+subtopic+"/"+title+'.txt', "w") as text_file:
+            with open(dir_to_save+"/"+title+'.txt', "w") as text_file:
                 text_file.write(text)
                 
             # As the file is written successfully, we will store this like to successfull list
@@ -154,7 +184,7 @@ def extraction(hyper_links):
             store_list_success.append(link)
             
             
-        except IOError:
+        except:
             print("Unable to open link: {}".format(link))
             
             # As we got no response from the webiste we are going to store the link in 'store_list_failure'
@@ -163,16 +193,7 @@ def extraction(hyper_links):
             store_list_failure.append(link)
             
             pass
-           
-            
-        
-        # Information for display
-        
-        #information = str(homedir+"/"+"Data/"+topic+"/"+subtopic+"/"+title+'.txt')    # Location of file saved
-        #print(information)
-        
-        
-        
+
 
 for index, links in enumerate(lines):
     resp = urllib.request.urlopen(links)
@@ -190,7 +211,7 @@ for index, links in enumerate(lines):
     # Creating links:
     print("Creating links for extraction...")
     
-    extraction_links = link_creation(links)      # Gets the list of links to extract contents from
+    extraction_links = link_creation(links,lang)      # Gets the list of links to extract contents from
         
     print("Link creation completet for topic:{} and subtopic:{}".format(topic, subtopic))
     
@@ -198,10 +219,15 @@ for index, links in enumerate(lines):
     print("Strating the extraction process...")
     
     #for index, links_2_extract in enumerate(extraction_links):
-    extraction(extraction_links)
+    extraction(extraction_links,lang)
     
     print("Extraction Process completed.")
     
+
+    
+
+
+# In[ ]:
 
 
 # Storing the 'store_list_success' and 'store_list_failure' into text file
